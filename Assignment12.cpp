f6b52263e8d0ef78f88f4fc199486e158d3eab5c
Prathamesh2642 <76043914+Prathamesh2642@users.noreply.github.com>
2022-05-22 08:16:04 +0530

    Create Assignment12.cpp

---
+++ Assignment12.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <unistd.h>
+using namespace std;
+
+void set_data()
+{
+
+}
+
+int main()
+{
+
+    ofstream o1;
+    int num;
+    string s1, no, index;
+
+    cout << "\nEnter no of entries: ";
+    cin >> num;
+
+    o1.open("Sample.txt");
+    cout << "\nWriting the File...\n";
+
+    cout << "\nFollowing format \n\t\t\tSr.No  Name  Roll No";
+
+    // getline(cin, s1);
+    // or below
+    getchar();
+    for (int i = 0; i < num; i++)
+    {
+        cout << "\nEnter student Detail: ";
+        getline(cin, s1);
+        o1 << s1 << endl;
+    }
+
+    cout << "Enter the index no. of the student: ";
+    cin >> no;
+    // Reading
+    ifstream i1;
+    i1.open("Sample.txt");
+
+    cout << "\nReading the File:\n";
+
+    while (i1)
+    {
+        getline(i1, s1);
+
+        index = s1[0];
+        if (no == index)
+        {
+            cout << "Found the record....\n";
+
+            cout << s1 << endl;
+            break;
+        }
+          else{
+    cout << "Not found !\n";
+    return 0;
+    }
+    }
+  
+}
